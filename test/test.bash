618ce50e60434a3be0a17716130529493baf949a
Yukimi Miyahara <yukimi_pooh@au.com>
2024-11-29 00:15:06 +0900

    solve

--- test/test.bash
+++ test/test.bash
@@ -9,8 +9,5 @@ source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-<<<<<<< HEAD
-grep 'Listen: 5'
-=======
 grep 'Listen: 10'
->>>>>>> f301fd810b4856bf72bea8692679885d521fc631
+
